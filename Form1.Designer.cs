3c2ebff00c8e8723360259167131c6c4dec68811
CharlieP37 <70582977+CharlieP37@users.noreply.github.com>
2024-05-10 21:04:47 -0600

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace VisualizationService
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.RefreshBtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.userlabel = new System.Windows.Forms.Label();
+            this.reportbtn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(27, 114);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(566, 310);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // RefreshBtn
+            // 
+            this.RefreshBtn.Location = new System.Drawing.Point(483, 63);
+            this.RefreshBtn.Name = "RefreshBtn";
+            this.RefreshBtn.Size = new System.Drawing.Size(110, 23);
+            this.RefreshBtn.TabIndex = 1;
+            this.RefreshBtn.Text = "Refrescar";
+            this.RefreshBtn.UseVisualStyleBackColor = true;
+            this.RefreshBtn.Click += new System.EventHandler(this.RefreshBtn_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 66);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(164, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Lista de jugadores activos";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(642, 222);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(197, 24);
+            this.comboBox1.TabIndex = 3;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // userlabel
+            // 
+            this.userlabel.AutoSize = true;
+            this.userlabel.Location = new System.Drawing.Point(639, 190);
+            this.userlabel.Name = "userlabel";
+            this.userlabel.Size = new System.Drawing.Size(200, 16);
+            this.userlabel.TabIndex = 4;
+            this.userlabel.Text = "Seleccione el usuario a bannear";
+            // 
+            // reportbtn
+            // 
+            this.reportbtn.Location = new System.Drawing.Point(642, 272);
+            this.reportbtn.Name = "reportbtn";
+            this.reportbtn.Size = new System.Drawing.Size(197, 23);
+            this.reportbtn.TabIndex = 5;
+            this.reportbtn.Text = "Reportar";
+            this.reportbtn.UseVisualStyleBackColor = true;
+            this.reportbtn.Click += new System.EventHandler(this.reportbtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(874, 450);
+            this.Controls.Add(this.reportbtn);
+            this.Controls.Add(this.userlabel);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.RefreshBtn);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Visualización";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button RefreshBtn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label userlabel;
+        private System.Windows.Forms.Button reportbtn;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using VisualizationService.ServiceReference1;
+
+namespace VisualizationService
+{
+    public partial class Form1 : Form
+    {
+        ServiceReference1.Player[] players;
+        GameVisualizationServiceSoapClient cliente = new ServiceReference1.GameVisualizationServiceSoapClient();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            players = cliente.listajugadores();
+        }
+
+        private void RefreshBtn_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Columns.Clear();
+
+            players = cliente.listajugadores();
+
+            dataGridView1.DataSource = players;
+            dataGridView1.Columns["skin"].Visible = false;
+            if (dataGridView1.Columns.Contains("status") == false) 
+            {
+                dataGridView1.Columns.Add("status", "status");
+                for (int i = 0; i < dataGridView1.Rows.Count; i++)
+                {
+                    dataGridView1.Rows[i].Cells[3].Value = "Online";
+                }
+            }
+
+            comboBox1.Items.Clear();
+
+            foreach (var p in players) 
+            {
+                comboBox1.Items.Add(p.username.ToString());
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void reportbtn_Click(object sender, EventArgs e)
+        {
+            var p = comboBox1.SelectedIndex;
+            var p1 = comboBox1.SelectedItem;
+            RefreshBtn_Click(null, new EventArgs());
+            var t = players.ToList();
+            //t.Remove(players.ElementAt(p-1));
+            players = t.ToArray();
+            comboBox1.Items.Remove(p1);
+        }
+    }
+}
